e7937b03cd8202bae9a040a607fc69a4a4cac297
CelestialBakery <kmm2559363@stu.o-hara.ac.jp>
2026-04-07 15:21:06 +0900

    feat
    変数を少し追加。

---
+++ PuzzleAction/Assets/HitBox/HitBox.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class HitBox : MonoBehaviour
+{
+    [SerializeField] private bool isHit;
+    [SerializeField] private Vector2 m_position;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CheckHit()
+    {
+
+    }
+    
+}
