9c6df499fef0b3e0059657eacfe5ca961e86c82a
NakaiMasahiro <Kmm2559305@stu.o-hara.ac.jp>
2026-04-07 16:38:32 +0900

    feat
    EffectData&EffectDataBace追加
    poolないと進まないのでいったん上げます

---
+++ PuzzleAction/Assets/EffectManager/Script/EffectData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[System.Serializable]
+public struct EffectData
+{
+    public string m_effectName;
+    public GameObject m_prefabEffect;
+    public int m_poolSize;
+
+    public float m_lifeTime; //0‚È‚çŽ©“®
+}
