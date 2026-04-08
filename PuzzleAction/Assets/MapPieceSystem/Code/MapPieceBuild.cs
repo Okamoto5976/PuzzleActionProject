dd9cdf3507fc1954c7727c6682f3ff02c6b5ddc0
Okamoto5976 <kmm2559327@stu.o-hara.ac.jp>
2026-04-09 00:15:40 +0900

    update:MapPieceBuild
    ピースを生成
    ピースをグリッドにおけるかどうか
    マスの壁が開いているかのデータ
    
    予定:ピースを外せるようにしたい
    隣のマスをみて　壁を開ける

--- PuzzleAction/Assets/MapPieceSystem/Code/MapPieceBuild.cs
+++ PuzzleAction/Assets/MapPieceSystem/Code/MapPieceBuild.cs
@@ -2,23 +2,67 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
 using System;
+using Unity.Android.Gradle.Manifest;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+
 
 public class MapPieceBuild : MonoBehaviour
 {
+    public class Cell
+    {
+        public bool up;
+        public bool down;
+        public bool left;
+        public bool right;
+    }
+
+    public class RoomShape
+    {
+        public List<Vector2Int> cells;
+    }
+
+
     private int m_gridWidth = 10;
     private int m_gridHeight = 10;
 
-    private struct Grid
+    Cell[,] m_grid;//二次元配列はx行y列の配列を用意　その中に型を入れれる
+    bool[,] m_isValid;
+
+    private void ReceiveMapData(int width, int height)
     {
-        public int x;
-        public int y;
+        m_gridWidth = width;
+        m_gridHeight = height;
     }
 
-    private List<Grid> m_grid = new List<Grid>();
 
     private void Start()
     {
-        
+        GridMap(m_gridWidth, m_gridHeight);
+        GridValidMap(m_gridWidth, m_gridHeight);
+    }
+
+    private void GridMap(int width, int height)//引数で受け取る
+    {
+        m_grid = new Cell[width, height];
+    }
+
+    private void GridValidMap(int width, int height)//床が有効かどうか
+    {
+        m_isValid = new bool[width, height];
+    }
+
+    private void CreatePiece()
+    {
+        var shap = new RoomShape();
+
+        shap.cells = new List<Vector2Int>
+        {
+            new Vector2Int(0,0),
+            new Vector2Int(1,0),
+            new Vector2Int(0,1),
+        };
+
     }
 
     private void CreateGurids()
@@ -32,8 +76,22 @@ public class MapPieceBuild : MonoBehaviour
         }
     }
 
-    private void BuildMapPiece()
+    private bool CanPlace(RoomShape shape, Vector2Int origin)
     {
+        foreach(var cell in shape.cells)
+        {
+            Vector2Int pos = origin + cell;
 
+            if(pos.x < 0 || pos.x >= m_gridWidth) return false;
+            if(pos.y < 0  || pos.y >= m_gridHeight) return false;
+
+            //gridがIsValidがfalse かどうか
+
+            if (m_grid[pos.x, pos.y] != null) return false;
+        }
+
+        return true;
     }
+    
+    
 }
