19fb4bac66348db0d70cb2147ec2bfa1c11deb44
ura-3d <kmm2559319@stu.o-hara.ac.jp>
2026-04-08 15:13:38 +0900

    EnmeyのState
    とりあえずコードだけ書いた
    明日試してみます

--- PuzzleAction/Assets/EnemySystem/Scene/state.cs
+++ PuzzleAction/Assets/EnemySystem/Scene/state.cs
@@ -1,16 +1,58 @@
 using UnityEngine;
 
-public class state : MonoBehaviour
+public class State : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // ===== 状態 =====
+    public enum EnemyState
     {
-        
+        Idle,
+        Chase,
+        Attack,
+        Damage,
+        Dead
     }
 
-    // Update is called once per frame
+    // 現在の状態
+    public EnemyState currentState = EnemyState.Idle;
+
+    // ===== フラグ =====
+    public bool isInvincible = false;
+    public bool canMove = true;
+    public bool canAttack = true;
+
     void Update()
     {
-        
+        DebugState(); // 確認用（あとで消してOK）
+    }
+
+    // ===== 状態変更用メソッド =====
+    public void ChangeState(EnemyState newState)
+    {
+        currentState = newState;
+    }
+
+    // ===== ダメージ管理だけ =====
+    public void SetInvincible(bool value)
+    {
+        isInvincible = value;
+    }
+
+    public void SetMove(bool value)
+    {
+        canMove = value;
+    }
+
+    public void SetAttack(bool value)
+    {
+        canAttack = value;
+    }
+
+    // ===== デバッグ（今の状態確認） =====
+    void DebugState()
+    {
+        Debug.Log("State: " + currentState +
+                  " | 無敵: " + isInvincible +
+                  " | 移動可: " + canMove +
+                  " | 攻撃可: " + canAttack);
     }
 }
