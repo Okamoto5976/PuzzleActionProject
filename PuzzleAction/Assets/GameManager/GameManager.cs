787dbf0c8df7b724360f5406405a33363f164909
ura-3d <kmm2559319@stu.o-hara.ac.jp>
2026-04-15 15:07:29 +0900

    add: Gamemanager初期化の追加
    初期化のコード記入途中

--- PuzzleAction/Assets/GameManager/GameManager.cs
+++ PuzzleAction/Assets/GameManager/GameManager.cs
@@ -5,52 +5,50 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameData data;
     [SerializeField] private GameRuntimeData runtime;
 
-    private bool isGameOver=false;//New
+    private bool isGameOver = false;
 
     void Start()
     {
-        //初期化
+        // 初期化
         runtime.SetScore(data.StartScore);
         runtime.SetMoney(data.StartMoney);
         runtime.SetTime(data.TimeLimit);
 
-        //リセット★
         Time.timeScale = 1f;
-
     }
 
     void Update()
     {
-        //ゲームオーバー後は止める★
+        //ゲームオーバー後に止める
         if (isGameOver) return;
 
         runtime.SetTime(runtime.Time - Time.deltaTime);
-        
-        if(runtime.Time <= 0)
+
+        //時間切れ
+        if (runtime.Time <= 0)
         {
             runtime.SetTime(0);
             GameOver();
         }
 
-        Debug.Log($"Score:{runtime.Score}|Money:{runtime.Money}|Time:{runtime.Time:F1}");
+        Debug.Log($"Score: {runtime.Score} | Money: {runtime.Money} | Time: {runtime.Time:F1}");
     }
 
-    //ゲームオーバー★
-    private void GameOver()
+    //ゲームオーバー
+    public void GameOver()
     {
         isGameOver = true;
         Debug.Log("ゲームオーバー");
         Time.timeScale = 0f;
     }
-
     //スコア
-    private void AddScore(int value)
+    public void AddScore(int value)
     {
         runtime.SetScore(runtime.Score + value);
     }
 
     //お金
-    private void AddMoney(int value)
+    public void AddMoney(int value)
     {
         runtime.SetMoney(runtime.Money + value);
     }
--- PuzzleAction/Assets/GameManager/GameRuntimeData.cs
+++ PuzzleAction/Assets/GameManager/GameRuntimeData.cs
@@ -3,16 +3,24 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "GameRuntimeData", menuName = "Game/RuntimeData")]
 public class GameRuntimeData : ScriptableObject
 {
-    [SerializeField] private int score; //スコア
-    [SerializeField] private int money; //お金
-    [SerializeField] private float time;  //時間
+    [SerializeField] private int score;
+    [SerializeField] private int money;
+    [SerializeField] private float time;
 
-    public int Score => score;
-    public int Money => money;
-    public float Time => time;
+    public int Score => score; //スコア
+    public int Money => money; //お金
+    public float Time => time; //時間
 
-    //値変更
-    public void SetScore(int value) =>score = value;
-    public void SetMoney(int value) =>money = value;
+    //初期化
+    public void Init(int startScore,int startMoney,float startTime)
+    {
+        score = startScore;
+        money = startMoney;
+        time = startTime;
+    }
+
+    // 値変更用
+    public void SetScore(int value) => score = value;
+    public void SetMoney(int value) => money = value;
     public void SetTime(float value) => time = value;
 }
