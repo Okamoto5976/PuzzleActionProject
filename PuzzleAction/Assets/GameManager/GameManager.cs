d05cadb5a4869b0b37980e4714523e746d020d04
ura-3d <kmm2559319@stu.o-hara.ac.jp>
2026-04-20 11:31:22 +0900

    update:Gamemanagerコード
    コードの書き換え

--- PuzzleAction/Assets/GameManager/GameManager.cs
+++ PuzzleAction/Assets/GameManager/GameManager.cs
@@ -6,12 +6,12 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameRuntimeData runtime;
 
     private bool m_isGameOver = false;
-
+    
     void Start()
     {
         runtime.Init(data.StartScore, data.StartMoney, data.TimeLimit);
         
-        //デバック用★
+        //デバック用
         Debug.Log("GameManagerが動く");
         Time.timeScale = 1f;
     }
@@ -23,31 +23,32 @@ public class GameManager : MonoBehaviour
 
         runtime.DecreaseTime(Time.deltaTime);
 
-        //時間切れ
+        //デバック用
+        //Debug.Log($"Score: {runtime.Score} | Money: {runtime.Money} | Time: {runtime.Time:F1}");
+
+        //時間切れ★
         if (runtime.Time <= 0)
         {
             GameOver();
         }
-        //デバック
-        Debug.Log($"Score: {runtime.Score} | Money: {runtime.Money} | Time: {runtime.Time:F1}");
     }
 
-    //ゲームオーバー
+    //ゲームオーバー★
     public void GameOver()
     {
         m_isGameOver = true;
 
-        //デバック用★
+        //デバック用
         Debug.Log("ゲームオーバー");
         Time.timeScale = 0f;
     }
-    //スコア加算
+    //スコア加算★
     public void AddScore(int m_value)
     {
         runtime.AddScore(m_value);
     }
 
-    //お金加算　
+    //お金加算★
     public void AddMoney(int m_value)
     {
         runtime.AddMoney(m_value);
