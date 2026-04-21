188423b8441fc357d7e5bf0c3266eafe12ce55d8
syuu375 <kmm2559313@stu.o-hara.ac.jp>
2026-04-21 15:03:45 +0900

    refactor:何度も繰り返しオブジェクトを呼ばないようにしただけ

--- PuzzleAction/Assets/Area/Areanokoukahoruda/AreaController.cs
+++ PuzzleAction/Assets/Area/Areanokoukahoruda/AreaController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem.Users;
 
 public enum AreaType
 {
@@ -10,21 +11,25 @@ public enum AreaType
 
 public class AreaController : MonoBehaviour
 {
-
     public AreaType m_CurrentArea;
-
-    //public void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.CompareTag("Player"))
-    //    {
-    //        ExecuteEffect();
-    //    }
-    //}
-
+    private bool m_Generate = false;
+     void Start()
+    {
+        Setup();
+    }
+    private void Setup()
+    {
+        AreaSet areaSet =GetComponent<AreaSet>();
+        if (areaSet != null)
+        {
+            areaSet.SetupRoom(m_CurrentArea, transform);
+        }
+    }
 
     public void ExecuteEffect()
     {
-        AreaSet areaSet = GetComponent<AreaSet>();
+         AreaSet areaSet = GetComponent<AreaSet>();
+
         switch (m_CurrentArea)
         {
             case AreaType.Damage:
@@ -40,49 +45,38 @@ public class AreaController : MonoBehaviour
                 }
                 break;
 
-           //ase AreaType.Summon:
-           //   //コンポーネント探し
-           //   Spawn SpawnScriput = GetComponent<Spawn>();
-           //   if (SpawnScriput != null)
-           //   {
-           //       SpawnScriput.ActivateSpawn();
-           //   }
-           //   else
-           //   {
-           //       Debug.LogWarning("スポーンのスクリプトが見つからんアタッチしてるか？");
-           //   }
-           //   break;
-           //
+            case AreaType.Summon:
+                //コンポーネント探し
+                Spawn SpawnScriput = GetComponent<Spawn>();
+                if (SpawnScriput != null)
+                {
+                    SpawnScriput.ActivateSpawn();
+                }
+                else
+                {
+                    Debug.LogWarning("スポーンのスクリプトが見つからんアタッチしてるか？");
+                }
+                break;
+
             case AreaType.Normal:
                 //コンポーネント探しいる？
                 Debug.Log("何もおこらない");
                 break;
 
-            //case AreaType.Shop:
-            //    //コンポーネント探し
-            //    shop shopScriput = GetComponent<shop>();
-            //    if (shopScriput != null)
-            //    {
-            //        shopScriput.ActivationShop();
-            //    }
-            //    else
-            //    {
-            //        Debug.Log("ショップのスクリプトが見つからんアタッチしてるか");
-            //    }
-            //    break;
-
             case AreaType.Shop:
+                //コンポーネント探し
                 shop shopScriput = GetComponent<shop>();
-                if (shopScriput != null) shopScriput.ActivationShop();
-                if (areaSet != null) areaSet.SetupRoom(AreaType.Shop, transform);
+                if (shopScriput != null)
+                {
+                    shopScriput.ActivationShop();
+                }
+                else
+                {
+                    Debug.Log("ショップのスクリプトが見つからんアタッチしてるか");
+                }
                 break;
-
-            case AreaType.Summon:
-                Spawn SpawnScriput = GetComponent<Spawn>();
-                if (SpawnScriput != null) SpawnScriput.ActivateSpawn();
-                if (areaSet != null) areaSet.SetupRoom(AreaType.Summon, transform);
-                break;
-
+               
+                
         }
     }
 }
