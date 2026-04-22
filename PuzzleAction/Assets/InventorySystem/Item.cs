2c5db1529efb437a8152b612c8059f9a19b71d7d
Okamoto5976 <kmm2559327@stu.o-hara.ac.jp>
2026-04-22 22:33:48 +0900

    Inventory Updata
    Inventoryに関して書いた（本ブランチと関係なし）

---
+++ PuzzleAction/Assets/InventorySystem/Item.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+    [System.Serializable]
+    public class Item
+    {
+        public string name;
+        public string description;
+        
+        public Item(string name, string description)
+        {
+            this.name = name;
+            this.description = description;
+        }
+    }
