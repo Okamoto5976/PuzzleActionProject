f4dee09165c523556e5445db9110db1904f259ef
Pu-ta0629 <Kmm2559353@stu.o-hara.ac.jp>
2026-05-18 11:45:24 +0900

    Updata

--- PuzzleAction/Assets/EntitiySystem/Player/PlayerData.cs
+++ PuzzleAction/Assets/EntitiySystem/Player/PlayerData.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName="PlayerData",menuName ="Game/Player Data")]
 public class PlayerData : ScriptableObject
 {
-  [SerializeField]private  float m_normalSpeed = 5f;
-  [SerializeField]private float m_dashSpeed = 10f;
-  [SerializeField]private int hotbarSize = 3;
+    [SerializeField] private float m_normalSpeed = 5f;
+    [SerializeField] private float m_dashSpeed = 10f;
+    [SerializeField] private int hotbarSize = 3;
     public int HotbarSize => hotbarSize;
     public float NormalSpeed => m_normalSpeed;
 
