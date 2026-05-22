e74265e96ed1ac93f3e6f78048105b912c365d83
Hirano <Kmm2559336@stu.o-hara.ac.jp>
2026-05-22 15:21:21 +0900

    data

--- PuzzleAction/Assets/ItemSystem/DropManager.cs
+++ PuzzleAction/Assets/ItemSystem/DropManager.cs
@@ -4,14 +4,4 @@ using UnityEngine;
 public class DropManager : MonoBehaviour
 {
     dropPool pool;
-    
-    public List<Item> DropItemList = new List<Item>();
-
-    // Update is called once per frame
-   //dropPool GetDropPool(Vector3 pos)
-   // {
-   //     int dropindex = Random.Range(0, DropItemList.Count);
-
-   //     //return ;
-   // }
 }
