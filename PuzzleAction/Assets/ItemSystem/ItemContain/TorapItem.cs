94af07cd8d5ed10137c49b756180440b81b5ffb4
CelestialBakery <kmm2559363@stu.o-hara.ac.jp>
2026-05-22 17:01:06 +0900

    fate
    トラップ設置機能を追加。（未完成）

--- PuzzleAction/Assets/ItemSystem/ItemContain/TorapItem.cs
+++ PuzzleAction/Assets/ItemSystem/ItemContain/TorapItem.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
 [CreateAssetMenu(fileName = "SpecialItem", menuName = "Scriptable Objects/Datas/SpecialItem")]
-public class TorapItem :Item
+public class TorapItem : Item
 {
     [SerializeField] private float torpvalue;
+    //trapType
+    //{arrow,gas
 
-  
-
+    //poolからもらったobj入れる 変数
+    public GameObject TrapPrefab;
 
     public override void Activation(float value, ItemRecieveData data)
     {
-        //Vector3 Corctorpos;
-        
-        
-
-        Debug.Log($"が罠の範囲に入って起動した");
+        //data posにobjを置く
+        //objにEntityがついている　Enityに dataのbaseValueを送る（コメントにする
+        //objのEntityにmoveDirがあるからdataのdirを入れる
+        var entity = TrapPrefab.GetComponent<Entity>();
+        entity.Initialize(ItemType.Weapon, data.baseValue);
+        //entity.moveDir = data.dir;
+        //GameObject obj = Instantiate(TrapPrefab, data.pos, Quaternion.LookRotation(data.dir));
     }
 }
