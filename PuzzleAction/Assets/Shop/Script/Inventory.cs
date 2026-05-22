646fa97566e33633b9b953c67cdadd7fcca27e18
ohr25375 <kmm2559375@stu.o-hara.ac.jp>
2026-05-22 09:49:34 +0900

    Revert "remove: shopsystem"
    This reverts commit 5e21618d9cf5f6cda57861f5156d0b006bf69326.

---
+++ PuzzleAction/Assets/Shop/Script/Inventory.cs
@@ -0,0 +1,73 @@
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Xml.Schema;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] public ItemSlot[,] slots = new ItemSlot[3, 3]; //マス生成
+    [SerializeField] private int maxStack = 10;
+    private void Awake()
+    {
+        //全マス初期化
+        for (int y = 0; y < 3; y++)
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                slots[y, x] = new ItemSlot();
+            }
+        }
+    }
+
+    //アイテムを追加する関数
+    public bool AddItem(int itemId)
+    {
+        //同じアイテムがあれば重ねる
+        for (int y = 0; y < 3; y++)
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                if (slots[y, x].m_itemId == itemId)
+                {
+                    if (slots[y, x].m_count < maxStack)
+                    {
+                        slots[y, x].m_count++;
+                        Debug.Log($"アイテム{itemId}を重ねた({slots[y, x].m_count}個)");
+                        return true;
+                    }
+                }
+            }
+        }
+        //空いてるマスを探す
+        for (int y = 0; y < 3; y++)
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                if (slots[y, x].m_itemId == 0)
+                {
+                    slots[y, x].m_itemId = itemId;
+                    slots[y, x].m_count = 1;
+                    Debug.Log($"新しいアイテム{itemId}を追加");
+                    return true;
+                }
+            }
+        }
+        //どこにも入らない
+        Debug.Log("インベントリがいっぱい");
+        return false;
+    }
+    public int GetItemCount(int itemId)
+    {
+        int total = 0;
+        for (int y = 0; y < 3; y++)
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                if (slots[y, x].m_itemId == itemId)
+                {
+                    total += slots[y, x].m_count;
+                }
+            }
+        }
+        return total;
+    }
+}
