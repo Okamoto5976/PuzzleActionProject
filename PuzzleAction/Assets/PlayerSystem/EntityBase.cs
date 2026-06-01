6e52c123875b44d93b3d8cf24f518cf36ea7682d
yasudayutaro <Kmm2559344@stu.o-hara.ac.jp>
2026-06-01 12:39:39 +0900

    mergetoenemyaiintomybranch

--- PuzzleAction/Assets/PlayerSystem/EntityBase.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[RequireComponent(typeof(EntityHP),typeof(EntityMove))]
-public class EntityBase : MonoBehaviour
-{
-    
-}
--- PuzzleAction/Assets/PlayerSystem/Yutaro/Script/ITeam.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-
-interface ITeam
-{
-
-}
