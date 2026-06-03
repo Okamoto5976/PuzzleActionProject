fb28c5e8e7b961afcde07a45afbec172f0643411
Hirano <Kmm2559336@stu.o-hara.ac.jp>
2026-06-03 14:23:47 +0900

    Update

--- PuzzleAction/Assets/ItemSystem/Itemdrop.cs
+++ PuzzleAction/Assets/ItemSystem/Itemdrop.cs
@@ -3,31 +3,53 @@ using UnityEngine;
 public class ItemDrop : MonoBehaviour
 {
     Item item; // アイテムのクラス
-    dropPool dropList; // ドロップするアイテムのリストのクラス
+    float radius = 3f; // アイテムを拾うための半径
+    float timeToReturn = 5f; // アイテムが自動的に戻るまでの時間
     dropPool pool; // アイテムを管理するドロッププールのクラス
     private GameObject prefab;
 
     //public event Action m_event;
-
-
-    //playerの座標か
-    //自身の半径３mいないに　プレイヤーが入ったら　プレイヤーにアイテムを渡す。
-    private void ItemGet(Collider othor)
+    void Update()
     {
-        if (othor.CompareTag("Player"))
+        if(Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) <= radius)
         {
-            Return();
+            AddToInventory();
         }
     }
+
+    void AddToInventory()
+    {
+        //InventoryManager .Instance.AddItem(item);
+        Destroy(gameObject);
+    }
+    //playerの座標か
+    //自身の半径３mいないに　プレイヤーが入ったら　プレイヤーにアイテムを渡す。
+    private void ItemGet(Collider other)
+    {
+        if (Vector3.Distance(transform.position, other.transform.position) <= radius)
+        {
+            if (pool == null)
+            {
+                Debug.LogError("Pool is not assigned.");
+                return;
+            }
+            if (other.CompareTag("Player"))
+            {
+                //Add.itemData;
+                Return();
+            }
+        }
+    }
+
     public void Initialize()
     {
-        Invoke(nameof(Return), 5f); //5秒後にReturnメソッドを呼び出す
+        Invoke(nameof(Return), timeToReturn); // timeToReturn秒後にReturnメソッドを呼び出す
     }
     private void Return()
     {
+
         if (pool != null)
         {
-
             //Poolに返す処理
             pool.ReturnItem(item, prefab);
         }
