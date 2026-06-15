dc0012baa1e1db66c3a22ca9546336db2c6d6a26
yasudayutaro <Kmm2559344@stu.o-hara.ac.jp>
2026-06-15 09:43:56 +0900

    update

--- PuzzleAction/Assets/EntitiySystem/Enemy/Enemy_Archer.cs
+++ PuzzleAction/Assets/EntitiySystem/Enemy/Enemy_Archer.cs
@@ -4,18 +4,10 @@ public class Enemy_Archer : MonoBehaviour, IEnemyBehaviour
 {
     private EnemyContllor m_controller;
 
-    private Entity m_entity;
-
     //[SerializeField] private ItemManager m_itemManager;
-    [SerializeField] private ArrowTrap m_arrowPrefab;
 
     private float m_coolTime = 1.5f;
     private float m_lastFireTime;
-
-    private void Awake()
-    {
-        m_entity = GetComponent<Entity>();
-    }
 
     public void Initialized(EnemyContllor controller)
     {
@@ -46,44 +38,10 @@ public class Enemy_Archer : MonoBehaviour, IEnemyBehaviour
 
     private void Shoot(Vector3 dir)
     {
-        //call Itemmanager method
-        //dir transform itemname
-
-        Use(dir);
-        //Instantiate(m_arrow, transform.position, new Quaternion(dir.x, dir.y, dir.z, 0));
+        m_controller.UseItem(dir);
     }
 
-    private void Use(Vector3 dir)
-    {
-        ArrowTrap arrow = Instantiate(
-            m_arrowPrefab
-            );
-
-        arrow.Init(m_entity, dir, 5);
-        arrow.transform.position = transform.position;
-
-        //TrapUseData data = new();
-
-        ////使用者
-        //data.Owner =
-        //    this.gameObject;
-
-        ////出現位置
-        //data.Position =
-        //    transform.position;
-
-        ////使用方向
-        //data.Direction = dir;
-        ////m_owner.transform.forward;
-        ////Debug.Log(m_owner.name);
-        ////Debug.Log(m_owner.transform.forward);
-
-        ////初期化
-        //arrow.Initialize(data);
-    }
-
-    public void Stop()
-    {
+    public void Stop(){
 
     }
 }
