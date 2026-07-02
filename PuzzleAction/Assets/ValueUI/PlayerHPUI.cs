5aa99ad7754f2d54bf8ef550380f11d8f0f32014
ura-3d <kmm2559319@stu.o-hara.ac.jp>
2026-07-02 11:40:58 +0900

    fix:HPUI統合

--- PuzzleAction/Assets/ValueUI/PlayerHPUI.cs
+++ PuzzleAction/Assets/ValueUI/PlayerHPUI.cs
@@ -5,19 +5,41 @@ using UnityEngine.UI;
 public class PlayerHPUI : MonoBehaviour
 {
     [Header("Reference")]
+    //å„Ç≈ê›íËÇ∑ÇÈ
     [SerializeField] private PlayerHP m_playerHP; 
 
     [Header("PlayerHP UI")]
-    [SerializeField] private Slider m_HPSlider;
+   // [SerializeField] private Slider m_HPSlider;
+    [SerializeField] private Image m_FrontHPImage;
+    [SerializeField] private Image m_BackHPImage;
     [SerializeField] private TMP_Text m_HPText;
 
-    public void Update()
+    [Header("Slider Speed")]
+    [SerializeField] private float m_BackSpeed = 0.5f;
+
+    private void Update()
     { 
         if (m_playerHP == null)
             return;
 
-        m_HPSlider.maxValue = m_playerHP.MaxHP;
-        m_HPSlider.value = m_playerHP.CurrentHP;
+        int maxHP = m_playerHP.MaxHP;
+        int currentHP = m_playerHP.CurrentHP;
+
+        float hoRate = (float)currentHP / maxHP;
+
+        if(m_FrontHPImage != null)
+        {
+            m_FrontHPImage.fillAmount = hoRate;
+        }
+
+        if(m_BackHPImage != null)
+        {
+            m_BackHPImage.fillAmount = Mathf.MoveTowards(
+                m_BackHPImage.fillAmount,
+                hoRate,
+                m_BackSpeed * Time.deltaTime
+                );
+        }
 
         if (m_HPText != null)
         {
