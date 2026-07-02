a2cb5be4a0b6bebb75b894eadcb86ca7458d1320
syuu375 <kmm2559313@stu.o-hara.ac.jp>
2026-07-02 14:38:22 +0900

    box

--- PuzzleAction/Assets/MapObject/Object Destruction.cs
+++ PuzzleAction/Assets/MapObject/Object Destruction.cs
@@ -2,15 +2,45 @@ using UnityEngine;
 
 public class ObjectDestruction : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private int maxHitCount = 1;
+    private int currentHitCount;
+    [SerializeField]private GameObject itemPrefab;
 
-    // Update is called once per frame
-    void Update()
+     void Start()
     {
-        
+        currentHitCount = maxHitCount;   
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            RegisterHit();
+        }
+    }
+    private void RegisterHit()
+    {
+        currentHitCount--;
+        Debug.Log($"{gameObject.name}にヒット！残り耐久:{currentHitCount}");
+        if (currentHitCount <= 0)
+        {
+            DestroyBox();
+        }
+    }
+    private void DestroyBox()
+    {
+        if (itemPrefab != null)
+        {
+            //dropdrop
+            //DropManager.Instance.SpawnItem(itemPrefab,transform.position);
+
+            //ObjectPool
+            //GameObject item=objectPool.Rent(itemPrefab);
+            //item.transform.position=transform.position;
+
+            Debug.Log($"{gameObject.name}が破壊されました!アイテムをドロップします");
+        }
+        Destroy(gameObject);
+        Debug.Log($"{gameObject.name}が破壊されました!何も出ませんシケです");
+        //gameObject.SetActive(false);//pool
     }
 }
