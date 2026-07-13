09dcf822b7acce65f76627393e8bb5e3d2af3af0
Okamoto5976 <kmm2559327@stu.o-hara.ac.jp>
2026-07-13 14:33:53 +0900

    refactor : UIManager

--- PuzzleAction/Assets/GameManager/UIController.cs
+++ PuzzleAction/Assets/GameManager/UIController.cs
@@ -1,50 +1,66 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UIController : MonoBehaviour
 {
+    private InputSystem_Actions m_action;
+
     [SerializeField] private GameObject m_gameOverUI;
+    [SerializeField] private GameObject m_gameClearUI;
     [SerializeField] private GameObject m_menuUI;
-    [SerializeField] private GameObject m_optionUI;
     [SerializeField] private GameObject m_shopUI;
     [SerializeField] private GameObject m_inventoryUI;
-    [SerializeField] private GameObject m_gameClearUI;
 
-    [Header("Event")]
-    [SerializeField] private BoolEventSO m_gameOverUIEvent;
-    [SerializeField] private BoolEventSO m_menuUIEvent;
-    [SerializeField] private BoolEventSO m_optionUIEvent;
-    [SerializeField] private BoolEventSO m_shopUIEvent;
-    [SerializeField] private BoolEventSO m_inventoryUIEvent;
-    [SerializeField] private BoolEventSO m_gameClearUIEvent;
+    //[Header("Event")]
+    //[SerializeField] private BoolEventSO m_gameOverUIEvent;
+    //[SerializeField] private BoolEventSO m_menuUIEvent;
+    //[SerializeField] private BoolEventSO m_optionUIEvent;
+    //[SerializeField] private BoolEventSO m_shopUIEvent;
+    //[SerializeField] private BoolEventSO m_inventoryUIEvent;
+    //[SerializeField] private BoolEventSO m_gameClearUIEvent;
 
-    private bool isMenuOpen = false;
-    private bool isInventoryOpen = false;
+    private bool m_isMenu = false;
+    private bool m_isInventory = false;
+    //private bool isInventoryOpen = false;
 
     private void OnEnable()
     {
-        m_menuUIEvent.Register(MenuUI);
-        m_gameOverUIEvent.Register(GameOverUI);
-        m_optionUIEvent.Register(OptionUI);
-        m_shopUIEvent.Register(ShopUI);
-        m_inventoryUIEvent.Register(InventoryUI);
-        m_gameClearUIEvent.Register(ShowGameClearUI);
+
+
+        //m_menuUIEvent.Register(MenuUI);
+        //m_gameOverUIEvent.Register(GameOverUI);
+        //m_optionUIEvent.Register(OptionUI);
+        //m_shopUIEvent.Register(ShopUI);
+        //m_inventoryUIEvent.Register(InventoryUI);
+        //m_gameClearUIEvent.Register(ShowGameClearUI);
     }
 
     private void OnDisable()
     {
-        m_gameOverUIEvent.Unregister(GameOverUI);
-        m_menuUIEvent.Unregister(MenuUI);
-        m_optionUIEvent.Unregister(OptionUI);
-        m_shopUIEvent.Unregister(ShopUI);
-        m_inventoryUIEvent.Unregister(InventoryUI);
-        m_gameClearUIEvent.Unregister(ShowGameClearUI);
+        //m_gameOverUIEvent.Unregister(GameOverUI);
+        //m_menuUIEvent.Unregister(MenuUI);
+        //m_optionUIEvent.Unregister(OptionUI);
+        //m_shopUIEvent.Unregister(ShopUI);
+        //m_inventoryUIEvent.Unregister(InventoryUI);
+        //m_gameClearUIEvent.Unregister(ShowGameClearUI);
+
+        m_action.Disable();
+
     }
 
     private void Start()
     {
+        m_action = new InputSystem_Actions();
+
+        m_action.Player.Menu.performed += ToggleMenu;
+        m_action.Player.Inventory.performed += ToggleInventory;
+
+        m_action.Enable();
+
+
         m_gameOverUI.SetActive(false);
         m_menuUI.SetActive(false);
-        m_optionUI.SetActive(false);
+        //m_optionUI.SetActive(false);
         if(m_shopUI != null) m_shopUI.SetActive(false);
         m_inventoryUI.SetActive(false);
         m_gameClearUI.SetActive(false);
@@ -53,81 +69,95 @@ public class UIController : MonoBehaviour
     private void Update()
     {
         // ESC�Ń��j���[
-        if (Input.GetKeyDown(KeyCode.Escape))
+        //if (Input.GetKeyDown(KeyCode.Escape))
+        //{
+        //    m_IsMenu = !m_IsMenu;
+
+        //    //m_menuUIEvent.Raise(isMenuOpen);
+
+        //    // ���j���[������I�v�V����������
+        //    if (!m_IsMenu)
+        //    {
+        //        //m_optionUIEvent.Raise(false);
+        //    }
+        //}
+
+        //// TAB�ŃC���x���g��
+        //if (Input.GetKeyDown(KeyCode.Tab))
+        //{
+        //    //isInventoryOpen = !isInventoryOpen;
+
+        //    //m_inventoryUIEvent.Raise(isInventoryOpen);
+        //}
+    }
+
+    //if get key "EscapeKey"
+    private void ToggleMenu(InputAction.CallbackContext context)
+    {
+        if(!m_isMenu)
         {
-            isMenuOpen = !isMenuOpen;
+            OnShowMenuUI(true);
+            Time.timeScale = 0f;
 
-            m_menuUIEvent.Raise(isMenuOpen);
+        }
+        else
+        {
+            OnShowMenuUI(false);
+            Time.timeScale = 1f;
 
-            // ���j���[������I�v�V����������
-            if (!isMenuOpen)
-            {
-                m_optionUIEvent.Raise(false);
-            }
         }
 
-        // TAB�ŃC���x���g��
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            isInventoryOpen = !isInventoryOpen;
+    }
 
-            m_inventoryUIEvent.Raise(isInventoryOpen);
+    private void ToggleInventory(InputAction.CallbackContext callback)
+    {
+        if(!m_isInventory)
+        {
+            OnShowInventoryUI(true);
+        }
+        else
+        {
+            OnShowInventoryUI(false);
         }
     }
 
-    private void GameOverUI(bool isbool)
+
+    public void OnShowGameOverUI(bool isbool)
     {
+        if (m_gameOverUI == null) return;
+
         m_gameOverUI.SetActive(isbool);
     }
 
-    private void MenuUI(bool isbool)
+    public void OnShowGameClearUI(bool isbool)
     {
+        if (m_gameClearUI == null) return;
+
+        m_gameClearUI.SetActive(isbool);
+    }
+
+    public void OnShowMenuUI(bool isbool)
+    {
+        if (m_menuUI == null) return;
+        //menu Isoption = true , menu run method
+        m_isMenu = isbool;
         m_menuUI.SetActive(isbool);
     }
 
-    private void OptionUI(bool isbool)
-    {
-        m_optionUI.SetActive(isbool);
-    }
 
-    private void ShopUI(bool isbool)
+    public void OnShowShopUI(bool isbool)
     {
+        if (m_shopUI == null) return;
+
         //m_shopUI.SetActive(isbool);
     }
 
-    private void InventoryUI(bool isbool)
+    public void OnShowInventoryUI(bool isbool)
     {
+        if (m_inventoryUI == null) return;
+
+        m_isInventory = isbool;
+
         m_inventoryUI.SetActive(isbool);
-    }
-
-    private void ShowGameClearUI(bool isShow)
-    {
-        m_gameClearUI.SetActive(isShow);
-    }
-
-    // �V���b�v�J��
-    public void OnShopUI()
-    {
-        m_shopUIEvent.Raise(true);
-    }
-
-    // �C���x���g���J��
-    public void OnInventoryUI()
-    {
-        m_inventoryUIEvent.Raise(true);
-    }
-
-    // �I�v�V�����J��
-    public void OpenOption()
-    {
-        m_menuUI.SetActive(false);
-        m_optionUI.SetActive(true);
-    }
-
-    // ���j���[�ɖ߂�
-    public void BackMenu()
-    {
-        m_optionUI.SetActive(false);
-        m_menuUI.SetActive(true);
     }
 }
