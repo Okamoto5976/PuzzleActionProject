e5562b8dbbd4fca25062b8c272941bb8abfb88ec
ohr25375 <kmm2559375@stu.o-hara.ac.jp>
2026-09-02 12:21:47 +0900

    Initial implementation of worm

---
+++ PuzzleAction/Assets/Team_Enemy's/Enemy_Worm/EnemyWormController.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+
+[System.Serializable]
+public class EnemyWormController
+{
+    private float detectDistance;
+    private float attackAnimationTime;
+    private float attackAnimationCooldown;
+    private Transform transform;
+    private float time;
+    private EnemyController enemyController;
+    private bool hasAttacked;
+    private enum WormState
+    {
+        Relocate,
+        Standby,
+        Trigger
+    }
+
+    private WormState state;
+
+    public void Initialize(EnemyController enemyController, Transform transform, float detectDistance, float attackAnimationTime, float attackAnimationCooldown)
+    {
+        state = WormState.Relocate;
+        this.enemyController = enemyController;
+        this.transform = transform;
+        this.detectDistance = detectDistance;
+        this.attackAnimationTime = attackAnimationTime;
+        this.attackAnimationCooldown = attackAnimationCooldown;
+    }
+
+    public void DoWormState()
+    {
+        time += Time.deltaTime;
+        switch (state)
+        {
+            case WormState.Relocate: DoRelocate(); break;
+            case WormState.Standby: DoStandby(); break;
+            case WormState.Trigger: DoTrigger(); break;
+        }
+    }
+
+    private void DoRelocate()
+    {
+        //var position = enemyController.GetRandomPosition();
+        //enemyController.TeleportToPosition(position);
+        ChangeState(WormState.Standby);
+    }
+
+    private void DoStandby()
+    {
+        if (enemyController.Target == null) return;
+
+        var targetPosition = enemyController.Target.Value;
+        float distance = Vector3.Distance(transform.position, targetPosition);
+        if (distance <= detectDistance)
+        {
+            ChangeState(WormState.Trigger);
+            hasAttacked = false;
+        }
+    }
+
+    private void DoTrigger()
+    {
+        if (time >= attackAnimationTime)
+        {
+            enemyController.Attack();
+            hasAttacked = true;
+        }
+        if (time >= attackAnimationTime + attackAnimationCooldown)
+        {
+            ChangeState(WormState.Relocate);
+        }
+    }
+
+    private void ChangeState(WormState state)
+    {
+        this.state = state;
+        time = 0;
+    }
+}
