87be62d20cce4cdcafba0d90defadf407647146d
CelestialBakery <kmm2559363@stu.o-hara.ac.jp>
2026-09-14 17:06:43 +0900

    update

--- PuzzleAction/Assets/Team_Enemy's/Enemy_Demon/EnemyDemonController.cs
+++ PuzzleAction/Assets/Team_Enemy's/Enemy_Demon/EnemyDemonController.cs
@@ -1,7 +1,6 @@
-using System;
 using UnityEngine;
 
-public class EnemyDemonController : MonoBehaviour
+public class EnemyDemonController
 {
     private enum DemonState
     {
@@ -31,16 +30,27 @@ public class EnemyDemonController : MonoBehaviour
     private StepDirection stepDir;
     private ChasingAction action;
     private Transform transform;
+    private Rigidbody rb;
 
     private float time;
 
-    private float stepCooldown;
     private float probabilityOfTakeStep;
     private float nextActionDuration;
+    private float stepPower;
 
 
     private float DistanceToTarget => Vector3.Distance(transform.position, enemyController.Target.Value);
 
+    public void Initialize(EnemyController enemyController, Transform transform, float probablityOfTakeStep, float nextActionDuration, float stepPower)
+    {
+        this.enemyController = enemyController;
+        this.transform = transform;
+        this.probabilityOfTakeStep = probablityOfTakeStep;
+        this.nextActionDuration = nextActionDuration;
+        this.stepPower = stepPower;
+        time = 0;
+        enemyController.SetIsInvincible(true);
+    }
 
     public void DoDemonStates()
     {
@@ -54,19 +64,44 @@ public class EnemyDemonController : MonoBehaviour
 
     private void DoChasing()
     {
-
+        if (enemyController.Target == null) return;
+        if (DistanceToTarget <= enemyController.AttackRange)
+        {
+            SetState(DemonState.Attack);
+            return;
+        }
     }
 
     private void DoAttack()
     {
+        enemyController.Stop();
+        enemyController.Attack();
+        float lotteryTakeStep = Random.Range(0, 100);
+        if (lotteryTakeStep < probabilityOfTakeStep)
+        {
+            SetState(DemonState.Step);
+            return;
+        }
 
+        if (DistanceToTarget > enemyController.AttackRange)
+        {
+            SetState(DemonState.Chasing);
+        }
     }
 
 
 
     private void TakeStep(StepDirection stepDir)
     {
-
+        enemyController.SetEnableRotation(false);
+        Vector3 stepForce = Vector3.zero;
+        switch (stepDir)
+        {
+            case StepDirection.Back: stepForce = -Vector3.forward * stepPower + Vector3.up; break;
+            case StepDirection.Left: stepForce = -Vector3.right * stepPower + Vector3.up; break;
+            case StepDirection.Right: stepForce = Vector3.right * stepPower + Vector3.up; break;
+        }
+        rb.AddForce(stepForce);
     }
 
     private void AfterTheStep()
--- PuzzleAction/Assets/Team_Enemy's/Enemy_Demon/Enemy_Demon.cs
+++ PuzzleAction/Assets/Team_Enemy's/Enemy_Demon/Enemy_Demon.cs
@@ -7,16 +7,16 @@ public class Enemy_Demon : MonoBehaviour, IEnemyBehaviour
     private EnemyController m_enemyController;
     private EnemyDemonController m_enemyDemonController = new();
 
-    [SerializeField] private float stepCooldown = 0.5f;
-    [SerializeField] private float probabilityOfTakeStep = 75f;  // probability of take step
+    [SerializeField] private float probabilityOfTakeStep = 75f;
     [SerializeField] private float nextActionDurationMin = 1f;
     [SerializeField] private float nextActionDurationMax = 5f;
+    [SerializeField] private float stepPower = 1f;
 
 
     public void Initialized(EnemyController enemyController)
     {
         m_enemyController = enemyController;
-        //m_enemyDemonController.Initialize(enemyController, transform, preAttackDuration, postAttackDuration);
+        m_enemyDemonController.Initialize(enemyController, transform, probabilityOfTakeStep, Random.Range(nextActionDurationMin, nextActionDurationMax), stepPower);
     }
 
     public void Execute()
