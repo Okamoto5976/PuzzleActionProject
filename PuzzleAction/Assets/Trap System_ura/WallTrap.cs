69565b67dfb8b143fd99bb7a0e1eca15876b0458
ura-3d <kmm2559319@stu.o-hara.ac.jp>
2026-09-16 13:57:47 +0900

    refactor : 壁魔法の改善

--- PuzzleAction/Assets/Trap System_ura/WallTrap.cs
+++ PuzzleAction/Assets/Trap System_ura/WallTrap.cs
@@ -7,59 +7,28 @@ public class WallTrap : TrapBase
     [SerializeField] private float m_spawnDistance = 3.0f;
     [SerializeField] private float m_lifeTime = 5.0f;
 
-    [Header("Spawn")]
-    [SerializeField] private float m_spawnDelay = 1.0f;
-
-    [Header("KnockBack")]
-    [SerializeField] private float m_knockBackPower = 5.0f;
-
 
     protected override void EntitySetUp()
     {
         
     }
-
-
+    
     public override void TrapInit()
     {
         base.TrapInit();
 
         m_dir = transform.forward;
 
-        if (m_spawnDelay > 0.0f)
-        {
-            StartCoroutine(SpawnDelay());
-        }
-        else
-        {
-            Spawn();
-        }
-    }
-
-
-    private IEnumerator SpawnDelay()
-    {
-        yield return new WaitForSeconds(m_spawnDelay);
-
         Spawn();
     }
 
-
     private void Spawn()
     {
-        m_rb.linearVelocity = Vector3.zero;
-        m_rb.angularVelocity = Vector3.zero;
-
         transform.position +=
             m_dir * m_spawnDistance;
-        OnAddForce(
-            m_dir,
-            m_knockBackPower
-        );
 
         StartCoroutine(ReturnAfterTime());
     }
-
 
     private IEnumerator ReturnAfterTime()
     {
@@ -67,7 +36,6 @@ public class WallTrap : TrapBase
 
         OnReturnPool();
     }
-
 
     protected override void OnHit()
     {
